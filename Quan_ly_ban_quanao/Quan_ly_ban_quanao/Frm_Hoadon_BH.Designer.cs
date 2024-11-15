6103a96b9f004a039f218e9ddb173989f0ed6827
namdao2310 <namdao0987203@gmail.com>
2024-11-15 10:37:38 -0330

    .

---
+++ Quan_ly_ban_quanao/Quan_ly_ban_quanao/Frm_Hoadon_BH.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Quan_ly_ban_quanao
+{
+    partial class Frm_Hoadon_BH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Hoadon_BH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 531);
+            this.Name = "Frm_Hoadon_BH";
+            this.Text = "Frm_Hoadon_BH";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Quan_ly_ban_quanao/Quan_ly_ban_quanao/Frm_Hoadon_BH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_ly_ban_quanao
+{
+    public partial class Frm_Hoadon_BH : Form
+    {
+        public Frm_Hoadon_BH()
+        {
+            InitializeComponent();
+        }
+    }
+}
